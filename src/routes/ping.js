87b18a141eac5a174873747a29daa1099dbfdf67
= <animesh7995@gmail.com>
2025-10-22 20:22:43 +0530

    minor fix

--- src/routes/ping.js
+++ src/routes/ping.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.status(200).json({message: "Ping From Backend Server"})
+  res.status(200).json({message: "Ping From Backend Server"});
 });
 
 module.exports = router;
