8b44630e0211a4f85c1368d16ff4bc73231c0605
Subhrajeet <prakashparijat@gmail.com>
2025-11-01 22:01:44 +0530

    minor fix

--- src/routes/login.js
+++ src/routes/login.js
@@ -18,7 +18,8 @@ router.post('/', async function (req, res, next) {
     res.cookie("token", authToken, {
         httpOnly: true,
         secure: process.env.ENV==="PROD",
-        sameSite: process.env.ENV==="PROD"?"strict":"lax",
+        sameSite: "none",
+        //sameSite: process.env.ENV==="PROD"?"strict":"lax",
         maxAge: 60 * 60 * 1000,
     });
     return res.status(200).json({
