795d238f268f585d0967cd120efda949f2f92e8b
= <animesh7995@gmail.com>
2025-11-02 13:35:57 +0530

    delete product router

--- src/routes/deleteProduct.js
+++ src/routes/deleteProduct.js
@@ -3,21 +3,21 @@ const tables = require('../data/DynamoDBTables');
 const Database = require('../config/DynamoDB');
 const express = require('express');
 const router = express.Router();
-router.post('/', Token.verifyToken, async function(req,res,next) {
+router.post('/:productId', Token.verifyToken, async function(req,res,next) {
     const userId = req.userId;
     const userRole = req.userRole;
-    const productDetails = req.body;
+    const productId = req.params.productId;
     if(!userId || userRole !== "admin")
         return res.status(401).json({message:'User Authorization failed'});
-    if(!productDetails.product_id)
+    if(!productId)
         return res.status(400).json({message:'Bad Request'});
     const allProducts = await Database.executeScanCommand(tables.PRODUCTS);
-    const validProduct = allProducts.find(product => product.product_id === productDetails.product_id);
+    const validProduct = allProducts.find(product => product.product_id === productId);
     if(!validProduct)
         return res.status(401).json({message:'Product Not Found'});
-    const response = await Database.executeDeleteCommand(tables.PRODUCTS, {product_id:productDetails.product_id});
+    const response = await Database.executeDeleteCommand(tables.PRODUCTS, {product_id:productId});
     const updatedAllProducts = await Database.executeScanCommand(tables.PRODUCTS);
     if(response.httpStatusCode === 200)
-        return res.status(200).json({message: "Product deleted successfully",updatedAllProducts});
+        return res.status(200).json({message: "Product deleted successfully",product: updatedAllProducts});
 });
 module.exports = router;
